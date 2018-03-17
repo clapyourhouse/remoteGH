b098dd76fae4b13b5945a77cbb72e7d32e2c5833
ShogoKitamura <shogokitamura@ShogoKitamura-no-MacBook-Air.local>
2018-03-18 04:20:26 +0900

    plistへ音声認識のやつ入れました。swift始めました。

--- RemotoGoogleHome/ViewController.swift
+++ RemotoGoogleHome/ViewController.swift
@@ -7,13 +7,44 @@
 //
 
 import UIKit
+import Speech
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBAction func recordButtonTapped(_ sender: Any) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 録音開始を無効化する。
+        recordButton.isEnabled = false
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    //viewWillAppearとかに書く
+    override func viewWillAppear(_ animated: Bool) {
+            SFSpeechRecognizer.requestAuthorization { (status) in
+                OperationQueue.main.addOperation {
+                    switch status {
+                    case .authorized:   // 許可OK
+                        self.recordButton.isEnabled = true
+                        self.recordButton.backgroundColor = UIColor.blue
+                    case .denied:       // 拒否
+                        self.recordButton.isEnabled = false
+                        self.recordButton.setTitle("録音許可なし", for: .disabled)
+                    case .restricted:   // 限定
+                        self.recordButton.isEnabled = false
+                        self.recordButton.setTitle("このデバイスでは無効", for: .disabled)
+                    case .notDetermined:// 不明
+                        self.recordButton.isEnabled = false
+                        self.recordButton.setTitle("録音機能が無効", for: .disabled)
+                    }
+                }
+            }
+    }
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ja-JP"))!
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
